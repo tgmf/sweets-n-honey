53cb30950d521dbec9fe193337b9a037f9638821
tgmf <2.tagmf@gmail.com>
2020-04-15 21:04:07 +0300

    renaming default.html to index.html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require("http").Server(app);
 var port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/default.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 http.listen(port, function () {
